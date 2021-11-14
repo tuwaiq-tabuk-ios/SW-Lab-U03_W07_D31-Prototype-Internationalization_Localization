f4cc940d494df74fcb8a3db26cb5ff309e74caa7
Shorouq AlAnzi <shorouqalanzi@MacBook-Air-Shorouq.local>
2021-11-15 01:04:36 +0300

    Created Localization

---
+++ Prototype-Navigation-Table-ShuruqAlanazi/Prototype-Navigation-Table-ShuruqAlanazi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype-Navigation-Table-ShuruqAlanazi
+//
+//  Created by Shorouq AlAnzi on 09/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
